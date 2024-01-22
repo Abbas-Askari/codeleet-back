584ce9b042c3ad90b740d308d8d7a0d3361f0d0f
Abbas-Askari <abbasaskari797@gmail.com>
2024-01-22 17:34:02 +0500

    listen after db connections

--- app.js
+++ app.js
@@ -18,21 +18,21 @@ app.use(express.static("public"));
 app.use("/submissions", submissionsRouter);
 app.use("/problems", problemsRouter);
 app.use("/users", usersRouter);
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log({ error });
-  });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log({ error });
+  });
 
 export default app;
